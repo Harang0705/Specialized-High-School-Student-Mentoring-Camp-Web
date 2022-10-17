0fa9d53ca83f074b96c1d63ca30d54b6717f7687
Harang0705 <t62711691@gmail.com>
2022-10-17 20:32:24 +0900

    라라벨 버전 변경 및 기초 퍼블리싱

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+// document.getElementById('scroll').addEventListener('mouseover', function (event) {
+//     var d = document.getElementById('scroll');
+
+//     console.log(d.offsetHeight);
+//     // if (myDiv.offsetHeight + myDiv.scrollTop >= myDiv.scrollHeight) {
+//     //     scrolledToBottom(e);
+//     //   }
+// });
