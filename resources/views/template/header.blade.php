11949a7131ca354fd7be18fae4da09cb92e9adef
Harang0705 <t62711691@gmail.com>
2022-10-22 15:12:45 +0900

    header axios cdn 추가

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @include('template.css')
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script> <!-- axios -->
     <title>#찾다</title>
 </head>
